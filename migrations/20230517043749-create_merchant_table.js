25801c165d5e3f08e89113017c4c848bb8c6aed5
bimadewantoro <bimadewantoro@students.unnes.ac.id>
2023-05-17 17:22:44 +0700

    feat: [BD] add upload csvfile and store to database

---
+++ migrations/20230517043749-create_merchant_table.js
@@ -0,0 +1,78 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('merchants', {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        merchant_name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        merchant_description: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        merchant_city: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        merchant_postalcode: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        created_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        },
+        updated_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        },
+        merchant_action: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+        },
+        action_date: {
+            type: Sequelize.DATE,
+            allowNull: true,
+        },
+        merchant_status: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false,
+            defaultValue: true,
+        },
+        approved_by: {
+            type: Sequelize.INTEGER,
+            allowNull: true,
+        },
+        rejected_by: {
+            type: Sequelize.INTEGER,
+            allowNull: true,
+        }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('merchants');
+  }
+};
--- routes/file.route.js
+++ routes/file.route.js
@@ -5,12 +5,20 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
+        cb(null, './public/uploads/');
+    }
+});
+
+const storageCsv = multer.diskStorage({
+    destination: function (req, file, cb) {
         cb(null, './public/uploads/csv');
     }
 });
 
 const upload = multer({storage: storage});
+const uploadCsv = multer({storage: storageCsv});
 
 router.post('/', upload.single('file'), file.uploadFile);
+router.post('/csv', uploadCsv.single('csv'), file.uploadCsvFile);
 
 module.exports = router;
